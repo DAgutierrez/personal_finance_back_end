7c74dae0245cbd361898a8fa1bbdec125995a650
diego <diego.gutierrez684@gmail.com>
2025-01-21 22:21:05 -0300

    add changes telescope

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth:sanctum'])->group(function () {
     Telescope::routes();
 });
