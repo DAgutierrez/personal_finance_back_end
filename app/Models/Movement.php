15740b5d6195ed33fa608a40990063d87efb80b7
diego <diego.gutierrez684@gmail.com>
2025-01-22 18:41:31 -0300

    alter groupByCategory

--- app/Models/Movement.php
+++ app/Models/Movement.php
@@ -72,7 +72,7 @@ class Movement extends Model
 
     public static function groupByCategory($data)
     {
-        $movements = Movement::where('process_date', $data['process_date'])->groupBy('movement_category_id');
+        $movements = Movement::where('process_date', $data['process_date'])->where('payment_method_id', $data['payment_method_id'])->groupBy('movement_category_id');
 
         $keys =  $movements->keys();
 
